a8b61385c3bcd34941adf676f088bc6e6923dfb2
Maciek <azalut@gmail.com>
2014-11-18 00:03:24 +0100

    import organize in whole project

--- src/main/java/com/shrmusic/service/user/UserService.java
+++ src/main/java/com/shrmusic/service/user/UserService.java
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
