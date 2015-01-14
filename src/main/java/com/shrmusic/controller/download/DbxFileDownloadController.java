dc0b795173dd1714e3a19cdea2ed1f88dead5903
Maciej <azalut@gmail.com>
2015-01-14 23:43:35 +0100

    a stopgap of sending multiple files from controller: they are packed in zip file; MUST be corrected and refactored; in pom.xml IOUtils from apachecommons added and other removed

--- src/main/java/com/shrmusic/controller/download/DbxFileDownloadController.java
+++ src/main/java/com/shrmusic/controller/download/DbxFileDownloadController.java
@@ -1,6 +1,7 @@
 package com.shrmusic.controller.download;
 
 import com.shrmusic.service.download.DbxDownloadService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping(value = "account/{id}/download")
@@ -33,5 +36,38 @@ public class DbxFileDownloadController {
         response.setContentType("audio/mpeg3");
         response.setHeader("Content-Disposition", "attachment; filename=" + filename);
         return file;
+    }
+
+    @RequestMapping(value = "/zip", produces = "application/zip")
+    public byte[] zipFiles(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.addHeader("Content-Disposition", "attachment; filename=\"test.zip\"");
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(bufferedOutputStream);
+
+        ArrayList<File> files = new ArrayList<>(2);
+        files.add(new File("README.md"));
+
+        for (File file : files) {
+            zipOutputStream.putNextEntry(new ZipEntry(file.getName()));
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            IOUtils.copy(fileInputStream, zipOutputStream);
+
+            fileInputStream.close();
+            zipOutputStream.closeEntry();
+        }
+
+        if (zipOutputStream != null) {
+            zipOutputStream.finish();
+            zipOutputStream.flush();
+            IOUtils.closeQuietly(zipOutputStream);
+        }
+        IOUtils.closeQuietly(bufferedOutputStream);
+        IOUtils.closeQuietly(byteArrayOutputStream);
+
+        return byteArrayOutputStream.toByteArray();
     }
 }
